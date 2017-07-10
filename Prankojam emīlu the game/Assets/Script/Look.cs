ebd154e6cfdd00942b06cfbb7d1cb10947e63ce3
Matīss <matissisme@gmail.com>
2017-07-11 01:50:19 +0300

    Added the project files

---
+++ Prankojam emīlu the game/Assets/Script/Look.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Look : MonoBehaviour {
+	public float mouseSensitivity = 1000.0f;
+	public float clampAngle = 80.0f;
+
+	float rotY = 0.0f;
+	float rotX = 0.0f;
+
+	void Start() {
+		Vector3 rot = transform.localRotation.eulerAngles;
+		rotY = rot.y;
+		rotX = rot.x;
+	}
+
+	void Update() {
+		float mouseX = Input.GetAxis ("MouseX");
+		float mouseY = - Input.GetAxis ("MouseY"); // -
+
+		rotY += mouseX * mouseSensitivity * Time.deltaTime;
+		rotX += mouseY * mouseSensitivity * Time.deltaTime;
+
+		rotX = Mathf.Clamp (rotX, -clampAngle, clampAngle);
+
+		Quaternion localRotation = Quaternion.Euler (rotX, rotY, 0.0f);
+		transform.rotation = localRotation;
+	}
+}
---
+++ Prankojam emīlu the game/Assets/Script/Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	public float mspeed = 20.0f;
+
+	void Start() {
+		Cursor.visible = false;
+	}
+
+	void Update () {
+		var x = - Input.GetAxis("Horizontal") * Time.deltaTime * 15.0f;
+		var z = - Input.GetAxis("Vertical") * Time.deltaTime * mspeed;
+
+		transform.Translate(x, 0, 0);
+		transform.Translate(0, 0, z);
+
+		if (Input.GetKey (KeyCode.LeftShift)) {
+			mspeed = 40.0f;
+		} else {
+			mspeed = 20.0f;
+		}
+
+		if (Input.GetKey(KeyCode.Escape)) {
+			Application.Quit();
+		}
+	}
+}
