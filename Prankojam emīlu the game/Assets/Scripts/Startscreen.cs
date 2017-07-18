59693d110ccf6a4e87a4b081ee864aad3ca54bec
Matīss <matissisme@gmail.com>
2017-07-18 22:23:29 +0300

    pre-alpha 0.09, 0.091 build
    Added start menu

---
+++ Prankojam emīlu the game/Assets/Scripts/Startscreen.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Startscreen : MonoBehaviour {
+
+	public Button QuitB;
+	public Button StartB;
+
+	void Start() {
+		Button quit = QuitB.GetComponent<Button> ();
+		quit.onClick.AddListener (Quit);
+
+		Button start = StartB.GetComponent<Button> ();
+		start.onClick.AddListener (StartGame);
+	}
+
+	void Quit() {
+		Application.Quit ();
+	}
+	void StartGame() {
+		SceneManager.LoadScene ("game");
+	}
+}
