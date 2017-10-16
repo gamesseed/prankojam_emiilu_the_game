f36eef3edb33f50bf9d3490e5cf2090e89c783cb
Matīss <matissisme@gmail.com>
2017-10-16 16:14:17 +0300

    aigariino

---
+++ Prankojam emīlu the game/Assets/Scripts/ai.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ai : MonoBehaviour {
+	public Transform player;
+	void Update() {
+		transform.LookAt (player, Vector3.up);
+	}
+}
